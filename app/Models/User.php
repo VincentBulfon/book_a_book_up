53d46640272d91a37f22764dc0fb3b9920070a23
vincent <vincent.bulfon@gmail.com>
2021-09-06 00:31:55 +0200

    Disabling fortify verification email

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,9 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 
-class User extends Authenticatable implements MustVerifyEmail
+//disable verification
+//implements MustVerifyEmail
+class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
